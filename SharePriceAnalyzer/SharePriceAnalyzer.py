e85f33ac6201fabbb45c4af6f82c641c0ac1f79e
TimGAllan <59513050+TimGAllan@users.noreply.github.com>
2022-08-04 08:50:01 +0100

    Added exitCheck Function

--- SharePriceAnalyzer/SharePriceAnalyzer.py
+++ SharePriceAnalyzer/SharePriceAnalyzer.py
@@ -5,30 +5,37 @@ import time
 import keyboard
 import sys
 
-StockSymbols = ['ADEN.SW','RAND.AS','MAN','HAS.L','RHI','KELYA']
-
-for x in StockSymbols:
-	info.FetchStockInfo(x)
-	sh.FetchStockPriceHistoryDay(x)
-
-i = 0
-
-while True:
-		
-	if i <= 0:
-		for x in StockSymbols:	
-			sh.FetchStockPriceHistoryMinute(x)
-
-		i = 60
-
-	i -= 1
-	
-	if i % 5 == 0 and i>=1:
-		print(dt.datetime.now().strftime("%H:%M:%S"), "Updating Live prices in", i, "seconds...")
-
-	if keyboard.read_key() == "esc":
+def exitCheck():
+	if keyboard.is_pressed("esc"):
 		print(dt.datetime.now().strftime("%H:%M:%S"), "Stopping...")
-		sys.exit()		
+		sys.exit()
 
-	time.sleep(1)
+def Main():
+	StockSymbols = ['ADEN.SW','RAND.AS','MAN','HAS.L','RHI','KELYA']
 
+	for x in StockSymbols:
+		info.FetchStockInfo(x)
+		sh.FetchStockPriceHistoryDay(x)
+		exitCheck()
+
+	i = 0
+
+	while True:
+		
+		if i <= 0:
+			for x in StockSymbols:	
+				sh.FetchStockPriceHistoryMinute(x)
+				exitCheck()
+
+			i = 60
+	
+		i -= 1
+	
+		if i % 5 == 0 and i>=1:
+			print(dt.datetime.now().strftime("%H:%M:%S"), "Updating Live prices in", i, "seconds...")
+
+		exitCheck()	
+
+		time.sleep(1)
+
+Main()
